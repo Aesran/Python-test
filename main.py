35b21eb44acb576aaa22e8d876ee13298979c458
Aesran <bole.ludovic@outlook.com>
2017-09-23 22:09:56 +0200

    Ajout du fichier main() pour le programme Zcasino avec la boucle de base.

---
+++ main.py
@@ -0,0 +1,16 @@
+# Fichier main()
+# Différentes étapes pour faire fonctionner le programme
+
+# Boucle qui continue tant que le jeu n'est pas terminé
+continuer = true
+
+while continuer = true:
+
+  key = input('Envoyer :quit pour quitter')
+
+  if key = ":quit":
+    continuer = false
+  
+
+  if continuer = false:
+    break
