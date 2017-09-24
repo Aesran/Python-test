f0f4b4024ab9b7fb29817463d49c5d5b3c646b53
Aesran <bole.ludovic@outlook.com>
2017-09-24 11:11:02 +0200

    - Remaniement du code pour integrer les classes sur le fichier principale

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 # FIchier principal du jeu
 # Boucle qui continue tant qu'on a pas decide de quitter le jeu
+class player:
+# Attributs
+
+  def __init__(self, somme):
+    print(somme)
+    self.argent = somme
+    self.mise = 0
+    self.couleur = 'rouge'
+    self.chiffre = 0
+    self.nom = 'Joueur'
+    print(self.argent)
+
+  def monargent(self):
+        print(self.argent)
+
+
+
+# DEBUT DU PROGRAMME
+joueur = player(100)
+joueur.monargent()
+
 
 continuer = True
 while continuer:
@@ -9,3 +30,8 @@ while continuer:
         continuer = False
     if not continuer:
         break
+
+    #Si le joueur a miser ->
+
+
+    # Si le joueur n'as ni miser, ni quitter ->
