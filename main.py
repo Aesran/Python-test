e530789db7f80e5b4c884e0434c7bdddfeedced4
Aesran <bole.ludovic@outlook.com>
2017-09-26 13:57:58 +0200

    - Ajout d'un veritable texte d'entrée - test pour récuperer une valeur au lieu d'une chaine de caractères

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# FIchier principal du jeu
-# Boucle qui continue tant qu'on a pas decide de quitter le jeu
+
+# CLASSE UTILISÉE POUR LE JEU
 class player:
 # Attributs
 
@@ -19,19 +19,33 @@ class player:
 
 # DEBUT DU PROGRAMME
 joueur = player(100)
-joueur.monargent()
 
 
+# Boucle qui continue tant qu'on a pas decide de quitter le jeu
 continuer = True
 while continuer:
-    key = input("Utilisez la commande @quit pour quitter le jeu")
-    key = key
+    print("Bienvenu dans le Casino d'Aesran")
+    print("A tout moment, vous pouvez utiliser la commande @quit pour quitter")
+    print("Le but de ce jeu est de trouver la bonne valeur de la roulette,")
+    print("Ou au moins sa couleur (rouge pour un chiffre pair, noir pour impair)")
+    print("Entrez simplement des chiffres pour miser !")
+    print("###########################################")
+
+    print("Vous avez ", joueur.argent ," euros en banque")
+
+    key_test = int('machin')
+    print(type(key_test))
+    key = input()
+
     if key == '@quit':
         continuer = False
     if not continuer:
         break
 
     #Si le joueur a miser ->
-
+    if isinstance(key, int):
+        print("c'est un nombre !")
 
     # Si le joueur n'as ni miser, ni quitter ->
+    else:
+        print("Vous n'avez ni miser, ni quitter, alors on fait quoi ?!!")
--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 def test():
-  message = "Test de fonctionnement"
-  test = "test"
-  r-test= "r-test"
-  return message
+	print('Ceci est un test*')
 
+def bisextile():
+	annee = input('Saissisez l\'année que vous voulez verifier : ')
+	annee = int(annee)
+	if annee%4 == 0:
+		if annee%100 == 0:
+			if annee%400 == 0:
+				print('Annee bisextile en vue')
+			else:
+				print('Ce n\'est pas une année bisexetile :(')
+		else:
+			print('Année bisextile en vue !')
+	else:
+		print('Ce n\'est pas une année bisextile :(')
 
-print(message)
+bisextile()
