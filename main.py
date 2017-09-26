9fe9309a73622bdb39f3b27a0c09b6989978d4a3
Aesran <bole.ludovic@outlook.com>
2017-09-26 21:08:51 +0200

    - Le code fonctionne désormais !

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 
+# fonction primaires
+# fonction qui permet de verifier la couleur d'un chiffre
+import sys
+import random
+import time
+
+print(sys.version_info)
+def quelleCouleur(value):
+    if value%2 == 0:
+        return 'rouge'
+    else:
+        return 'noire'
+
+
+
+
+
 # CLASSE UTILISÉE POUR LE JEU
 class player:
 # Attributs
-
   def __init__(self, somme):
     print(somme)
     self.argent = somme
@@ -17,35 +33,67 @@ class player:
 
 
 
+
+
 # DEBUT DU PROGRAMME
 joueur = player(100)
 
 
 # Boucle qui continue tant qu'on a pas decide de quitter le jeu
 continuer = True
+partie = 0
 while continuer:
-    print("Bienvenu dans le Casino d'Aesran")
-    print("A tout moment, vous pouvez utiliser la commande @quit pour quitter")
-    print("Le but de ce jeu est de trouver la bonne valeur de la roulette,")
-    print("Ou au moins sa couleur (rouge pour un chiffre pair, noir pour impair)")
-    print("Entrez simplement des chiffres pour miser !")
-    print("###########################################")
+    if partie == 0:
+        print("Bienvenu dans le Casino d'Aesran")
+        print("A tout moment, vous pouvez utiliser la commande @quit pour quitter")
+        print("Le but de ce jeu est de trouver la bonne valeur de la roulette,")
+        print("Ou au moins sa couleur (rouge pour un chiffre pair, noir pour impair)")
+        print("Entrez 'miser' pour miser !")
 
+    partie += 1
+    print(partie)
+
+    print("###########################################")
     print("Vous avez ", joueur.argent ," euros en banque")
 
-    key_test = int('machin')
-    print(type(key_test))
-    key = input()
+
+    key = input('=. ')
 
     if key == '@quit':
         continuer = False
-    if not continuer:
-        break
 
     #Si le joueur a miser ->
-    if isinstance(key, int):
-        print("c'est un nombre !")
+    if key == 'miser':
+        print("Sur quel chiffre voulez vous miser ? De 1 à 50")
+        miseChiffre = int(input('=. '))
+        print('Combien voulez-vous misez ?')
+        print('Il vous reste', joueur.argent , ' euros en banque')
+        miseArgent = int(input('=. '))
+        print('Bien vous êtes prêt ? C\'est partit !')
+        # on choisi un nombre au hazard entre 1 et 50
+        hazard = random.randint(1,50)
+        # on annonce que la roulette tourne (et on fait patienter quelques secondes)
+        print('La roue tourne ...')
+        time.sleep(1)
 
-    # Si le joueur n'as ni miser, ni quitter ->
-    else:
-        print("Vous n'avez ni miser, ni quitter, alors on fait quoi ?!!")
+
+        # Puis on lances les conditions !
+        if hazard == miseChiffre:
+            nb = miseArgent * 2
+            joueur.argent += nb
+            print('Jackpot ! Vous gagnez ' , nb , ' euros')
+        elif quelleCouleur(miseChiffre) == quelleCouleur(hazard):
+            print('Même couleur, vous gardez votre mise')
+        else:
+            print('Perdu ! Vous perdez la mise')
+            joueur.argent = joueur.argent - miseArgent
+            if joueur.argent <= 0:
+                print("Vous n'avez plus d'argent, le jeu s'arrête")
+                break
+
+        # Si le chiffre est le même, sinon si le chiffre est de même couleur, sinon on perd tout
+
+
+
+    if not continuer:
+        break
